d26420fe31f2a4c93c16f3707341ea371f51315f
mythosxin <746121583@qq.com>
2020-02-22 17:57:09 +0800

    upd: init版本

---
+++ index.ts
@@ -0,0 +1,225 @@
+'use strict'
+
+interface Query {
+  [propName: string]: string
+}
+
+interface Hoegaarden {
+  /**
+   * 以对象字面量形式返回 url query 解析结果
+   * @param url
+   * @example
+   * parse('http://www.wwx.com?name=aaa&name=bbb&age=25')  // {name: ['aaa', 'bbb'], age: 25}
+   */
+  parse(url: string): Query
+
+  /**
+   * 获取 url 参数中key对应的value
+   * @param url
+   * @param key
+   * @example
+   * param('http://www.wwx.com?name=aaa&name=bbb&age=25','name')  // ['aaa', 'bbb']
+   */
+  param(url: string, key: string): string
+
+  /**
+   * 将 url、query 进行拼接
+   * @param obj
+   * @example
+   * NOTE 对象套对象，数组套数组，数组套对象请使用 post 进行请求
+   * stringify({name: ['aaa', 'bbb'], age: 25});  // 'age=25&name=aaa&name=bbb'
+   */
+  stringify(obj: object): string
+
+  /**
+   * 将 url、query 分开
+   * @param url
+   * @example
+   * parseUrl('http://www.wwx.com?name=aaa') // {url: 'http://www.wwx.com', query: {name: 'aaa'}}
+   */
+  parseUrl(url: string): Object
+  /**
+   * 截取 url 携带的参数
+   * @param url
+   * @example
+   * extract('http://www.wwx.com?name=aaa') // ‘name=aaa’
+   */
+  extract(url: string): string
+
+}
+
+function encoderFormatter(key: string) {
+
+  return (result: any, value: any) => {
+    if (value === undefined) {
+      return result;
+    }
+    if (value === null) {
+      return [...result, encodeURIComponent(key)];
+    }
+    return [...result, [encodeURIComponent(key), '=', encodeURIComponent(value)].join('')];
+  };
+}
+
+function parserFormatter(key: string, value: string, accumulator: any): string | Array<string | number | boolean> {
+  if (accumulator[key] === undefined) {
+    return accumulator[key] = value;
+  }
+  return accumulator[key] = [].concat(accumulator[key], value);
+};
+
+function splitOnFirst(argc: string, separator: string) {
+  if (!(typeof argc === 'string' && typeof separator === 'string')) {
+    throw new TypeError('请传入string类型参数');
+  }
+
+  if (separator === '') {
+    return [];
+  }
+
+  const separatorIndex = argc.indexOf(separator);
+
+  if (separatorIndex === -1) {
+    return [];
+  }
+
+  return [
+    argc.slice(0, separatorIndex),
+    argc.slice(separatorIndex + separator.length)
+  ];
+}
+
+function keysSorter(argc: any): any {
+  if (Array.isArray(argc)) {
+    return argc.sort();
+  }
+
+  if (typeof argc === 'object') {
+    return keysSorter(Object.keys(argc))
+      .sort((a: string, b: string) => Number(a) - Number(b))
+      .map((key: string) => argc[key]);
+  }
+
+  return argc;
+}
+
+function removeHash(url: string): string {
+  const hashStart = url.indexOf('#');
+  if (hashStart !== -1) {
+    url = url.slice(0, hashStart);
+  }
+
+  return url;
+}
+
+function param(url: string, key: string): string {
+  const obj = this.parse(url)
+  return obj[key] || ''
+}
+
+function parse(url: string): Query {
+
+  const ret = Object.create(null);
+
+  if (typeof url !== 'string') {
+    return ret;
+  }
+
+  url = url.trim()
+  const index = url.indexOf('?')
+  const queryString = index !== -1 ? url.slice(index + 1) : url
+
+
+  if (!queryString) {
+    return ret;
+  }
+
+  for (const param of queryString.split('&')) {
+    let [key, value] = splitOnFirst(param.replace(/\+/g, ' '), '=');
+    value = value === undefined ? null : decodeURIComponent(value);
+    parserFormatter(decodeURIComponent(key), value, ret);
+  }
+
+  for (const key of Object.keys(ret)) {
+    const value = ret[key];
+    if (typeof value === 'object' && value !== null) {
+      for (const k of Object.keys(value)) {
+        value[k] = value[k];
+      }
+    } else {
+      ret[key] = value;
+    }
+  }
+
+  return Object.keys(ret).sort().reduce((result, key) => {
+    const value = ret[key];
+    if (Boolean(value) && typeof value === 'object' && !Array.isArray(value)) {
+      result[key] = keysSorter(value);
+    } else {
+      result[key] = value;
+    }
+
+    return result;
+  }, Object.create(null));
+}
+
+function stringify(obj: { [propName: string]: string | number | boolean }): string {
+  if (!obj || Array.isArray(obj) || typeof obj !== 'object') {
+    return '';
+  }
+
+  const keys = Object.keys(obj);
+
+  return keys.map(key => {
+    const value = obj[key];
+
+    if (value === undefined) {
+      return '';
+    }
+
+    if (value === null) {
+      return encodeURIComponent(key);
+    }
+
+    if (Array.isArray(value)) {
+      for (let index in value) {
+        if (Array.isArray(value[index])) {
+          return alert('存在array嵌套array的情况，暂不支持，请使用post进行请求')
+        } else if (typeof value[index] === 'object') {
+          return alert('存在array嵌套object的情况，暂不支持，请使用post进行请求')
+        }
+      }
+
+      return value
+        .reduce(encoderFormatter(key), [])
+        .join('&');
+    }
+
+    if (typeof value === 'object') {
+      return alert('暂不支持object嵌套object，请使用post进行请求')
+    }
+
+    return encodeURIComponent(key) + '=' + encodeURIComponent(value);
+  }).filter(x => x.length > 0).join('&');
+};
+
+function parseUrl(url: string): Object {
+  return {
+    url: removeHash(url).split('?')[0] || '',
+    query: parse(extract(url))
+  };
+};
+
+function extract(url: string): string {
+  url = removeHash(url);
+  const queryStart = url.indexOf('?');
+  if (queryStart === -1) {
+    return '';
+  }
+
+  return url.slice(queryStart + 1);
+}
+
+const hoegaarden: Hoegaarden = { extract, param, parse, stringify, parseUrl }
+
+export default hoegaarden
---
+++ src/index.tsx
@@ -0,0 +1,194 @@
+import React, { Component } from 'react'
+import { render } from 'react-dom'
+import hoegaarden from '../index'
+import './style'
+
+export default class App extends Component {
+
+  state = {
+    parse: 'http://www.wwx.com?name=aaa&name=bbb&age=25',
+    parseResult: '{"age":"25","name":["aaa","bbb"]}',
+    param: 'http://www.wwx.com?name=aaa&name=bbb&age=25',
+    paramKey: 'name',
+    paramResult: '["aaa","bbb"]',
+    stringify: `{name: ['aaa', 'bbb'], age: 25}`,
+    stringifyResult: 'name=aaa&name=bbb&age=25',
+    parseUrl: 'http://www.wwx.com?name=aaa&name=bbb&age=25',
+    parseUrlResult: '{"url":"http://www.wwx.com","query":{"age":"25","name":["aaa","bbb"]}}',
+    extract: 'http://www.wwx.com?name=aaa&name=bbb&age=25',
+    extractResult: 'name=aaa&name=bbb&age=25',
+  }
+
+  render() {
+    return (
+      <div className='manage'>
+        <div>
+          <h1 className='title'>hoegaarden</h1>
+          <div className='manage__line'>
+            <span className='manage__line__title'>parse:</span>
+            <input
+              className='manage__line__input'
+              placeholder='parse'
+              onChange={e => {
+                this.setState({
+                  parse: e.target.value,
+                }, () => {
+                  try {
+                    this.setState({
+                      parseResult: JSON.stringify(hoegaarden.parse(this.state.parse)),
+                    })
+                  } catch (error) {
+                    this.setState({
+                      parseResult: null,
+                    })
+                  }
+                })
+              }}
+              value={this.state.parse}
+            />
+          </div>
+          <div className='manage__line'>
+            <span className='manage__line__title'>结果:</span>
+            {this.state.parseResult}
+          </div>
+          <div className='manage__line'>
+            <span className='manage__line__title'>param:</span>
+            <input
+              className='manage__line__input manage__line__input1'
+              placeholder='param'
+              onChange={e => {
+                this.setState({
+                  param: e.target.value,
+                }, () => {
+                  try {
+                    this.setState({
+                      paramResult: JSON.stringify(hoegaarden.param(this.state.param, this.state.paramKey)),
+                    })
+                  } catch (error) {
+                    this.setState({
+                      paramResult: null,
+                    })
+                  }
+                })
+              }}
+              value={this.state.param}
+            />
+            <input
+              className='manage__line__input manage__line__input2'
+              placeholder='paramKey'
+              onChange={e => {
+                this.setState({
+                  paramKey: e.target.value,
+                }, () => {
+                  try {
+                    this.setState({
+                      paramResult: JSON.stringify(hoegaarden.param(this.state.param, this.state.paramKey)),
+                    })
+                  } catch (error) {
+                    this.setState({
+                      paramResult: null,
+                    })
+                  }
+                })
+              }}
+              value={this.state.paramKey}
+            />
+          </div>
+          <div className='manage__line'>
+            <span className='manage__line__title'>结果:</span>
+            {this.state.paramResult}
+          </div>
+          <div className='manage__line'>
+            <span className='manage__line__title'>stringify:</span>
+            <input
+              className='manage__line__input'
+              placeholder='stringify'
+              onChange={e => {
+                this.setState({
+                  stringify: e.target.value,
+                }, () => {
+                  try {
+                    function get(text) {
+                      return text;
+                    }
+                    const res = eval('get(' + this.state.stringify + ')')
+                    this.setState({
+                      stringifyResult: hoegaarden.stringify(res),
+                    })
+                  } catch (error) {
+                    this.setState({
+                      stringifyResult: null,
+                    })
+                  }
+                })
+              }}
+              value={this.state.stringify}
+            />
+          </div>
+          <div className='manage__line'>
+            <span className='manage__line__title'>结果:</span>
+            {this.state.stringifyResult}
+            <pre></pre>
+          </div>
+          <div className='manage__line'>
+            <span className='manage__line__title'>parseUrl:</span>
+            <input
+              className='manage__line__input'
+              placeholder='parseUrl'
+              onChange={e => {
+                this.setState({
+                  parseUrl: e.target.value,
+                }, () => {
+                  try {
+                    this.setState({
+                      parseUrlResult: JSON.stringify(hoegaarden.parseUrl(this.state.parseUrl)),
+                    })
+                  } catch (error) {
+                    this.setState({
+                      parseUrlResult: null,
+                    })
+                  }
+                })
+              }}
+              value={this.state.parseUrl}
+            />
+          </div>
+          <div className='manage__line'>
+            <span className='manage__line__title'>结果:</span>
+            {this.state.parseUrlResult}
+          </div>
+          <div className='manage__line'>
+            <span className='manage__line__title'>extract:</span>
+            <input
+              className='manage__line__input'
+              placeholder='extract'
+              onChange={e => {
+                this.setState({
+                  extract: e.target.value,
+                }, () => {
+                  try {
+                    this.setState({
+                      extractResult: hoegaarden.extract(this.state.extract),
+                    })
+                  } catch (error) {
+                    this.setState({
+                      extractResult: null,
+                    })
+                  }
+                })
+              }}
+              value={this.state.extract}
+            />
+          </div>
+          <div className='manage__line'>
+            <span className='manage__line__title'>结果:</span>
+            {this.state.extractResult}
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+render(<App />, document.getElementById('root'))
+
